d7e03a66ad37dceabb9ed4d7754af9883a39d2f2
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-08-24 10:09:49 +0700

    awal

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>SMK NEGERI 1 CIREBON | PPLG - Menjadi Seorang Web Developer</title>
+    <link rel="icon" href="{{ url('img/logo.png') }}">
+</head>
+<body>
+
+</body>
+</html>
