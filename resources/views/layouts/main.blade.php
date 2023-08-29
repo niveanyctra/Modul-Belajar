0733e06b8cd1b2066acf98763a5f42dd2781daec
Hamadah2pointO <muhammadrozaan@gmail.com>
2023-08-29 14:36:03 +0700

    Fix Sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,25 +13,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- Overlay Scrollbars --}}
+    <link href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/styles/overlayscrollbars.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js"></script>
+
     <style>
         @import url(http://weloveiconfonts.com/api/?family=fontawesome);
     </style>
 </head>
 
-<body>
+<body data-overlayscrollbars-initialize>
     @include('layouts.navbar')
     <div class="">
         @yield('content')
     </div>
     @yield('sidebar')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script id="rendered-js">
-    $(function(){
-      $('.toggle-menu').click(function(e){
-        e.preventDefault();
-        $('.sidebar').toggleClass('toggled');
-      });
-    });
-</script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script id="rendered-js">
+        $(function() {
+            $('.toggle-menu').click(function(e) {
+                e.preventDefault();
+                $('.sidebar').toggleClass('toggled');
+            });
+        });
+    </script>
 </body>
+
 </html>
