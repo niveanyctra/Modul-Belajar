a69fee0fdc480d1a299604591d9e6f8163561d63
RafRizu <thisrafi10@gmail.com>
2023-08-30 12:20:22 +0700

    .

--- resources/views/kelas-mentor/html/detail.blade.php
+++ resources/views/kelas-mentor/html/detail.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <h5>{{$posts->title}}</h5>
 <a href="{{url('/mentor/'.$posts->users->id)}}" class="fw-light font-dark">by : {{$posts->users->name}}</a><br>
-<p>Release date {{$posts->created_at->format('F Y  ')}}</p>
-<p>Last Update {{date('d/m/Y', strtotime($posts->from_date))}}</p>
+<p>Release date {{$posts->created_at->format('F, Y')}}</p>
+<p>Last Update {{$posts->created_at->format('F, Y')}}</p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$posts->id_yt}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 <p>{{$posts->content}}</p>
 <div class="row">
