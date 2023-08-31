c731c8d5a13d90287412082f292f1838a7e90b94
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-08-31 09:35:07 +0700

    update latihan

---
+++ resources/views/layouts/latihan/sidebar/html.blade.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+    <nav class="nav-side">
+        <h3 class="sidebar-header">HTML Atribut</h3>
+            <a href="{{url('/latihan/html-atribut-1')}}" class="{{ request()->is('latihan/html-atribut-1') ? 'active' : '' }}">Latihan 1</a>
+            <a href="{{url('/latihan/html-atribut-2')}}" class="{{ request()->is('latihan/html-atribut-2') ? 'active' : '' }}">Latihan 2</a>
+            <a href="{{url('/latihan/html-atribut-3')}}" class="{{ request()->is('latihan/html-atribut-3') ? 'active' : '' }}">Latihan 3</a>
+    </nav>
+</aside>
