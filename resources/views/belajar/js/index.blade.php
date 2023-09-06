4d100c4d97ab87d3367501c4f181737e503171a0
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-09-06 15:12:44 +0700

    update belajar js-how to

--- resources/views/belajar/js/index.blade.php
+++ resources/views/belajar/js/index.blade.php
@@ -7,5 +7,84 @@
     <h1>JavaScript Pengenalan</h1>
     <a href="{{ url('/') }}" class="btn btn-success">Home</a>
     <a href="{{ url('/belajar/js/where-to') }}" class="btn btn-success">Selanjutnya</a>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Dapat Mengubah Konten HTML
+    </h3>
+    <p>
+        Salah satu dari banyak metode JavaScript HTML adalah <span class="bg-body-secondary text-danger">getElementById()</span> . <br><br>
+        Contoh di baawah ini "mencari" elemen HTML (dengan id="demo"), dan mengubah konten elemen HTML (innerHTML) menjadi "Halo JavaScript".
+    </p>
+    <h4>Contoh</h4>
+    <div class="bg-dark text-light p-3">
+        document.getElementById(<span class="text-success">"demo"</span>).innerHTML = <span class="text-success">"Halo JavaScript"</span>;
+    </div><br>
+    <div class="bg-warning p-4">
+        <p>JavaScript menerima tanda kutip ganda dan tunggal</p>
+    </div> <br>
+    <h4>Contoh</h4>
+    <div class="bg-dark text-light p-3">
+        document.getElementById(<span class="text-success">'demo'</span>).innerHTML = <span class="text-success">'Halo JavaScript'</span>;
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Dapat Mengubah Value Atribut HTML
+    </h3>
+    <p>
+        Dalam contoh dibawah ini JavaScript mengubah value dari atribut <span class="bg-body-secondary text-danger">src</span> (source / sumber) dari tag <span class="bg-body-secondary text-danger">&lt;img&gt;</span> :
+    </p>
+    <div class="bg-secondary text-light p-3">
+        <button onclick="document.getElementById('myImage').src='{{ asset('img/belajar/js/bola-lampu-nyala.gif') }}'">Turn on the light</button>
+
+        <img id="myImage" src="{{ asset('img/belajar/js/bola_lampu-mati.gif') }}" style="width:100px">
+
+        <button onclick="document.getElementById('myImage').src='{{ asset('img/belajar/js/bola_lampu-mati.gif') }}'">Turn off the light</button>
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Dapat Mengubah Style HTML (CSS)
+    </h3>
+    <p>
+        Mengubah style dari elemen HTML, adalah variasi dari mengubah atribut HTML :
+    </p>
+    <div class="bg-dark text-light p-3">
+        <p>document.getElementById("demo").style.fontSize = "35px";</p>
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Dapat Menyembunyikan Elemen HTML
+    </h3>
+    <p>
+        Menyembunyikan elemen HTML hanya bisa dirubah dengan mengganti style <span class="bg-body-secondary text-danger">display</span> :
+    </p>
+    <div class="bg-dark text-light p-3">
+        <p>document.getElementById("demo").style.display = "none";</p>
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Dapat Manampilkan Elemen HTML
+    </h3>
+    <p>
+        Menampilkan elemen HTML juga hanya bisa dirubah dengan mengganti style <span class="bg-body-secondary text-danger">display</span> :
+    </p>
+    <div class="bg-dark text-light p-3">
+        <p>document.getElementById("demo").style.display = "block";</p>
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Dapat Mencari Elemen HTML dengan Class
+    </h3>
+    <p>
+        JavaScript tidak hanya dapat menemukan elemen HTML melalui atribut <span class="bg-body-secondary text-danger">id</span> namun juga dapat melalui atribut <span class="bg-body-secondary text-danger">class</span> :
+    </p>
+    <h3>Contoh</h3>
+    <div class="bg-dark text-light p-3">
+        <p>document.getElementsByClassName("demo");</p>
+    </div><br>
+    <div class="bg-warning p-4">
+        <p><strong>Note :</strong> Namun perlu diketahui jika ingin mengubah style elemen HTML melalui <span class="bg-body-secondary text-danger">class</span> berbeda dengan <span class="bg-body-secondary text-danger">id</span></p>
+    </div>
+    <a href="{{ url('/') }}" class="btn btn-success">Home</a>
+    <a href="{{ url('/belajar/js/where-to') }}" class="btn btn-success">Selanjutnya</a>
 </section>
 @endsection
--- resources/views/belajar/js/js-where-to.blade.php
+++ resources/views/belajar/js/js-where-to.blade.php
@@ -7,5 +7,126 @@
     <h1>JavaScript Where To</h1>
     <a href="{{ url('/belajar/js') }}" class="btn btn-success">Sebelumnya</a>
     <a href="{{ url('/belajar/js/output') }}" class="btn btn-success">Selanjutnya</a>
+    <hr>
+    <h3 class="my-3">
+        Tag &lt;script&gt;
+    </h3>
+    <p>
+        Dalam HTML, kode JavaScript dimasukan diantara <span class="bg-body-secondary text-danger">&lt;script&gt;</span> dan <span class="bg-body-secondary text-danger">&lt;/script&gt;</span>.
+    </p>
+    <h4>Contoh</h4>
+    <div class="bg-dark text-light p-3">
+        <p>
+            &lt;script&gt; <br>
+            document.getElementById("demo").innerHTML = "My First JavaScript"; <br>
+            &lt;/script&gt;
+        </p>
+    </div><br>
+    <div class="bg-warning p-4">
+        JavaScript lama mungkin menggunakan tipe atribut seperti : &lt;script type="text/javascript"&gt;. <br>
+        Tipe atribut seperti itu sekarang tidak wajib. JavaScript merupakan bahasa default script dalam HTML.
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript Function dan Event
+    </h3>
+    <p>
+        <span class="bg-body-secondary text-danger">function</span> JavaScript adalah sebuah blok di kode JavaScript, yang hanya bisa di eksekusi ketika "dipanggil". <br><br>
+        Misalnya, sebuah function dapat dipanggil ketika sebuah <strong>event</strong> atau <strong>peristiwa</strong> terjadi, seperti ketika mengklik sebuah tombol.
+    </p>
+    <hr>
+    <h3 class="my-3">
+        JavaScript dalam &lt;head&gt; atau &lt;body&gt;
+    </h3>
+    <p>
+        Kamu dapat menempatkan sebuah script dalam dokumen HTML. <br><br>
+        Script dapat ditempatkan di bagian <span class="bg-body-secondary text-danger">&lt;body&gt;</span>, atau di bagian <span class="bg-body-secondary text-danger">&lt;head&gt;</span> pada halaman HTML, atau pada kedua bagian tersebut.
+    </p>
+    <hr>
+    <h3 class="my-3">
+        JavaScript pada &lt;head&gt;
+    </h3>
+    <p>
+        Pada contoh kali ini, sebuah <span class="bg-body-secondary text-danger">function</span> JavaScript ditempatkan pada bagian <span class="bg-body-secondary text-danger">&lt;head&gt;</span> pada halaman HTML. <br><br>
+        Function ini dipanggil ketika sebuah tombol diklik :
+    </p>
+    <div class="bg-dark text-light p-3">
+        &lt;!DOCTYPE html&gt; <br>
+        &lt;html&gt;
+        <div class="ps-4">
+
+            &lt;head&gt;
+            <div class="ps-4">
+
+                &lt;script&gt; <br>
+                function myFunction() {
+                    <div class="ps-4">
+                        document.getElementById("demo").innerHTML = "Paragraf Dirubah.";
+                    </div>
+                } <br>
+                &lt;/script&gt;
+            </div>
+            &lt;/head&gt;<br>
+            &lt;body&gt;
+            <div class="ps-4">
+
+                &lt;h1&gt;Demo JavaScript pada bagian Head&lt;/h1&gt; <br>
+                &lt;p <span class="text-danger">id</span><span class="text-success">="demo"</span>&gt;Paragraf&lt;/p&gt; <br>
+                &lt;button <span class="text-danger">type</span><span class="text-success">="button"</span> <span class="text-danger">onclick</span><span class="text-success">="myFunction()"</span>&gt;Cobalah&lt;/button&gt;
+            </div>
+            &lt;/body&gt;
+        </div>
+        &lt;/html&gt;
+    </div>
+    <hr>
+    <h3 class="my-3">
+        JavaScript pada &lt;body&gt;
+    </h3>
+    <p>
+        Pada contoh kali ini, sebuah <span class="bg-body-secondary text-danger">function</span> JavaScript ditempatkan pada bagian <span class="bg-body-secondary text-danger">&lt;body&gt;</span> pada halaman HTML. <br><br>
+        Function ini dipanggil ketika sebuah tombol diklik :
+    </p>
+    <div class="bg-dark text-light p-3">
+        &lt;!DOCTYPE html&gt; <br>
+        &lt;html&gt;
+        <div class="ps-4">
+
+            &lt;body&gt;
+            <div class="ps-4">
+
+                &lt;h1&gt;Demo JavaScript pada bagian Head&lt;/h1&gt; <br>
+                &lt;p <span class="text-danger">id</span><span class="text-success">="demo"</span>&gt;Paragraf&lt;/p&gt; <br>
+                &lt;button <span class="text-danger">type</span><span class="text-success">="button"</span> <span class="text-danger">onclick</span><span class="text-success">="myFunction()"</span>&gt;Cobalah&lt;/button&gt;
+            </div>
+            <div class="ps-4">
+
+                &lt;script&gt; <br>
+                function myFunction() {
+                    <div class="ps-4">
+                        document.getElementById("demo").innerHTML = "Paragraf Dirubah.";
+                    </div>
+                } <br>
+                &lt;/script&gt;
+            </div>
+            &lt;/body&gt;
+        </div>
+        &lt;/html&gt;
+    </div><br>
+    <div class="bg-warning p-3">
+        <p>Menempatkan script di bagian bawah elemen &lt;body&gt; akan meningkatkan kecepatan tampilan web, karena interpretasi script memperlambat tampilan.</p>
+    </div>
+    <hr>
+    <h3 class="my-3">
+        External JavaScript
+    </h3>
+    <p>
+        Script juga dapat ditempatkan di file eksternal. <br><br>
+        Script eksternal praktis bila kode yang sama digunakan di banyak halaman web berbeda. <br><br>
+        File JavaScript memiliki ekstensi file <strong>.js</strong>. <br><br>
+        Untuk menggunakan script eksternal, masukkan nama file script di atribut <span class="bg-body-secondary text-danger">src</span> (source / sumber) pada tag <span class="bg-body-secondary text-danger">&lt;script&gt;</span> :
+    </p>
+    <div class="bg-dark text-light p-3">
+        &lt;script <span class="text-danger">src</span><span class="text-success">="script.js"</span>&gt;&lt;/script&gt; <br>
+    </div>
 </section>
 @endsection
