0716d2d18d5e43b83de8c56b69023a2cf6d39a38
Hamadah2pointO <muhammadrozaan@gmail.com>
2023-09-06 09:13:04 +0700

    Nav to sticky

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg px-5 fixed-top">
+<nav class="navbar navbar-expand-lg px-5 sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
             <img src="{{ url('img/logo.png') }}" width="40" alt="">
