6ea46ad777843c0155d8710b2ebb39bacf88bf45
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-09-08 13:38:35 +0700

    update logo alur belajar, community

--- resources/views/alur-belajar/back-end/index.blade.php
+++ resources/views/alur-belajar/back-end/index.blade.php
@@ -3,6 +3,6 @@
 <h3 class="text-center">Roadmap Back-End</h3>
 <hr>
 <div class="container">
-    <img src="{{ asset('img/backend_no-bg.png') }}" alt="" width="100%">
+    <img src="{{ asset('img/backend.png') }}" alt="" width="100%">
 </div>
 @endsection
