0f23b955f364b93c022eb491f0e735c0ee09c91c
RafRizu <thisrafi10@gmail.com>
2023-09-08 14:12:48 +0700

    page belajar-js done ga bang?

--- resources/views/belajar/js/js-loop.blade.php
+++ resources/views/belajar/js/js-loop.blade.php
@@ -7,5 +7,39 @@
     <h1>JavaScript Loop</h1>
     <a href="{{ url('/belajar/js/switch') }}" class="btn btn-success">Sebelumnya</a>
     <a href="{{ url('/belajar/js') }}" class="btn btn-success">Selanjutnya</a>
+    <hr>
+    <p>
+        Loop berguna untuk menjalankan kode terus menerus dengan value berbeda tiap kali menjalankan kode. <br>
+        <br>
+        Loop biasanya digunakan untuk menampilkan banyak data hanya dengan menulis beberapa baris kode.
+    </p>
+    <h4 class="my-3">
+        Jenis loop pada JavaScript
+    </h4>
+    <ul>
+        <li><span class="mark">for</span> - mengulang blok kode beberapa kali</li>
+        <li><span class="mark">for/in</span> - menelusuri properti suatu objek</li>
+        <li><span class="mark">for/of</span> - mengulang nilai objek yang dapat diubah</li>
+        <li><span class="mark">while</span> - mengulang blok kode selama kondisi yang ditentukan benar</li>
+        <li><span class="mark">do/while</span> - juga mengulang blok kode selama kondisi yang ditentukan benar</li>
+    </ul>
+    <h4 class="my-3">Contoh sederhana loop</h4>
+    <p>Kita akan kesulitan jika ingin menampilkan banyak data seperti berikut :</p>
+    <div class="code">
+        text += siswa[0] + "&lt;br&gt;";<br>
+        text += siswa[1] + "&lt;br&gt;";<br>
+        text += siswa[2] + "&lt;br&gt;";<br>
+        text += siswa[3] + "&lt;br&gt;";<br>
+        text += siswa[4] + "&lt;br&gt;";<br>
+        text += siswa[5] + "&lt;br&gt;";
+    </div>
+<p>Kita bisa mempersingkat penulisan dengan cara :</p>
+<div class="code">
+    for (let i = 0; i < siswa.length; i++) { <br>
+        text += siswa[i] + "&lt;br&gt;";
+}
+</div>
+    <a href="{{ url('/belajar/js/switch') }}" class="btn btn-success">Sebelumnya</a>
+    <a href="{{ url('/belajar/js') }}" class="btn btn-success">Selanjutnya</a>
 </section>
 @endsection
--- resources/views/belajar/js/js-switch.blade.php
+++ resources/views/belajar/js/js-switch.blade.php
@@ -7,5 +7,86 @@
     <h1>JavaScript Switch</h1>
     <a href="{{ url('/belajar/js/if-else') }}" class="btn btn-success">Sebelumnya</a>
     <a href="{{ url('/belajar/js/loop') }}" class="btn btn-success">Selanjutnya</a>
+    <hr>
+    <p>
+        Pernyataan <span class="mark">switch</span> digunakan untuk melakukan aksi yang berbeda tergantung kondisi yang berbeda.
+    </p>
+    <h5 class="my-3">Cara kerja switch</h5>
+    <ul>
+        <li>Ekspresi switch hanya dievaluasi satu kali</li>
+        <li>Nilai ekspresi akan dibandingkan dengan nilai tiap kondisi</li>
+        <li>Jika tidak ada kecocokan maka akan mengeksekusi blok default</li>
+    </ul>
+    <hr>
+    <h3 class="my-3">Contoh</h3>
+    <div class="code">
+        const hari = new Date().getDay(); <br>
+        switch(hari){
+            <div class="ps-3">
+                case 0:
+                <div class="ps-3">
+                    hari = "Senin"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                case 1:
+                <div class="ps-3">
+                    hari = "Selasa"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                case 2:
+                <div class="ps-3">
+                    hari = "Rabu"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                case 3:
+                <div class="ps-3">
+                    hari = "Kamis"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                case 4:
+                <div class="ps-3">
+                    hari = "Jumat"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                case 5:
+                <div class="ps-3">
+                    hari = "Sabtu"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                case 6:
+                <div class="ps-3">
+                    hari = "Minggu"; <br>
+                    break;
+                </div>
+            </div>
+            <div class="ps-3">
+                default:
+                <div class="ps-3">
+                    hari = "Hari tidak diketahui"; <br>
+                    break;
+                </div>
+            </div>
+        }
+    </div>
+    <p>
+        Pada kode diatas kita mengembalikan nama hari berdasarkan ekspresi hari yang akan memberikan output berdasarkan kondisi yang berbeda dan jika tidak ada kondisi yang terpenuhi maka akan menjalankan blok kode yang ada di blok default.
+    </p>
+    <div class="note">
+        <strong>Note :</strong> Pastikan memberikan <span class="mark">break</span> pada akhir blok kode. Bila tidak, blok kode berikutnya akan tereksekusi walaupun kondisinya tidak memenuhi
+    </div>
+        <a href="{{ url('/belajar/js/if-else') }}" class="btn btn-success">Sebelumnya</a>
+    <a href="{{ url('/belajar/js/loop') }}" class="btn btn-success">Selanjutnya</a>
 </section>
 @endsection
