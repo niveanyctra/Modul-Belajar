8a328d084dd2d2aef168498261293ab41f6797b4
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-09-09 17:11:51 +0700

    update side nav latihan

--- resources/views/layouts/latihan/sidebar/html.blade.php
+++ resources/views/layouts/latihan/sidebar/html.blade.php
@@ -1,7 +1,7 @@
 <aside class="sidebar">
     <nav class="nav-side">
         <h3 class="sidebar-header">Latihan HTML</h3>
-            <a href="{{url('/latihan/html/basic')}}" class="basicNav {{ request()->is('latihan/html/basic') ? 'active' : '' }}{{ request()->is('hasil/html/basic') ? 'active' : '' }}">HTML Basic</a>
+            <a href="{{url('/latihan/html/basic')}}" class="basicNav {{ request()->is('latihan/html/basic') ? 'active' : '' }} {{ request()->is('hasil/html/basic') ? 'active' : '' }}">HTML Basic</a>
             <div onclick="soal1()" class="ps-5 link basic" style="display:none;">Soal 1</div>
             <div onclick="soal2()" class="ps-5 link basic" style="display:none;">Soal 2</div>
             <div onclick="soal3()" class="ps-5 link basic" style="display:none;">Soal 3</div>
@@ -12,7 +12,7 @@
             <div onclick="soal8()" class="ps-5 link basic" style="display:none;">Soal 8</div>
             <div onclick="soal9()" class="ps-5 link basic" style="display:none;">Soal 9</div>
             <div onclick="soal10()" class="ps-5 link basic" style="display:none;">Soal 10</div>
-            <a href="{{url('/latihan/html/menengah')}}" class="menengahNav {{ request()->is('latihan/html/menengah') ? 'active' : '' }}">HTML Menengah</a>
+            <a href="{{url('/latihan/html/menengah')}}" class="menengahNav {{ request()->is('latihan/html/menengah') ? 'active' : '' }} {{ request()->is('hasil/html/menengah') ? 'active' : '' }}">HTML Menengah</a>
             <div onclick="soal1()" class="ps-5 link menengah" style="display:none;">Soal 1</div>
             <div onclick="soal2()" class="ps-5 link menengah" style="display:none;">Soal 2</div>
             <div onclick="soal3()" class="ps-5 link menengah" style="display:none;">Soal 3</div>
@@ -23,7 +23,7 @@
             <div onclick="soal8()" class="ps-5 link menengah" style="display:none;">Soal 8</div>
             <div onclick="soal9()" class="ps-5 link menengah" style="display:none;">Soal 9</div>
             <div onclick="soal10()" class="ps-5 link menengah" style="display:none;">Soal 10</div>
-            <a href="{{url('/latihan/html/lanjutan')}}" class="lanjutanNav {{ request()->is('latihan/html/lanjutan') ? 'active' : '' }}">HTML Lanjutan</a>
+            <a href="{{url('/latihan/html/lanjutan')}}" class="lanjutanNav {{ request()->is('latihan/html/lanjutan') ? 'active' : '' }}  {{ request()->is('hasil/html/lanjutan') ? 'active' : '' }}">HTML Lanjutan</a>
             <div onclick="soal1()" class="ps-5 link lanjutan" style="display:none;">Soal 1</div>
             <div onclick="soal2()" class="ps-5 link lanjutan" style="display:none;">Soal 2</div>
             <div onclick="soal3()" class="ps-5 link lanjutan" style="display:none;">Soal 3</div>
--- resources/views/layouts/latihan/subnav.blade.php
+++ resources/views/layouts/latihan/subnav.blade.php
@@ -5,16 +5,16 @@
             <a href="{{ url('/latihan/html/basic') }}" class="{{ request()->is('latihan/html*') ? 'active' : '' }}">
                 <span>LATIHAN HTML</span>
             </a>
-            <a href="{{ url('/latihan/css') }}" class="{{ request()->is('latihan/css*') ? 'active' : '' }}">
+            <a href="{{ url('/latihan/css/basic') }}" class="{{ request()->is('latihan/css*') ? 'active' : '' }}">
                 <span>LATIHAN CSS</span>
             </a>
-            <a href="{{ url('/latihan/php') }}" class="{{ request()->is('latihan/php*') ? 'active' : '' }}">
+            <a href="{{ url('/latihan/php/basic') }}" class="{{ request()->is('latihan/php*') ? 'active' : '' }}">
                 <span>LATIHAN PHP</span>
             </a>
-            <a href="{{ url('/latihan/js') }}" class="{{ request()->is('latihan/js*') ? 'active' : '' }}">
+            <a href="{{ url('/latihan/js/basic') }}" class="{{ request()->is('latihan/js*') ? 'active' : '' }}">
                 <span>LATIHAN JAVASCRIPT</span>
             </a>
-            <a href="{{ url('/latihan/sql') }}" class="{{ request()->is('latihan/sql*') ? 'active' : '' }}">
+            <a href="{{ url('/latihan/sql/basic') }}" class="{{ request()->is('latihan/sql*') ? 'active' : '' }}">
                 <span>LATIHAN SQL</span>
             </a>
         </nav>
