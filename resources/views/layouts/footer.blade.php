8892c2668f6abde69f096ba0b1d2c01141f5d6ea
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-09-11 08:51:20 +0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,15 @@
     <div class="container-fluid">
         <div class="text-center">
             <h5>Follow Us</h5>
-        instagram discord youtube
+            <a href="https://www.instagram.com/rpl_smkn1crb/?hl=id" target="_blank" rel="noopener noreferrer">Logo instagram</a>
+            <a href="https://www.youtube.com/@RPLSMKN1CIREBON" target="_blank" rel="noopener noreferrer">Logo Youtube</a>
+            <a href="https://discord.gg/VqPZbNKrzD" target="_blank" rel="noopener noreferrer">Logo Discord</a>
         </div>
         <hr>
         <div class="row">
             <div class="col-lg-3 col-12">
+                <img src="{{ asset('img/logo.png') }}" alt="" class="img-fluid" width="100px">
+                <img src="{{ asset('img/neper.png') }}" alt="" class="img-fluid" width="100px">
                 <p>Jl. Perjuangan By Pass Sunyaragi, Cirebon, Indonesia 45132</p>
                 <p>Telp & Fax: +62-0231-480202</p>
                 <p>Email: info@smkn1-cirebon.sch.id</p>
@@ -15,19 +19,72 @@
             <div class="col-lg-8 col-12">
                 <div class="row">
                     <div class="col-4">
-                        Lihat Perjalanan Karir
+                        <h6>
+                            Lihat Perjalanan Karir
+                        </h6>
+                        <a href="{{ url('/alur-belajar/back-end') }}">Back End</a>
+                        <a href="{{ url('/alur-belajar/front-end') }}">Front End</a>
+                        <a href="{{ url('/alur-belajar/ux-designer') }}">UX Design</a>
                     </div>
                     <div class="col-4">
-                        Tool yang Sedang Ramai di Gunakan
+                        <h6>
+                            Belajar Bahasa Pemograman
+                        </h6>
+                        <a href="{{ url('/belajar/html') }}">
+                            <span>Belajar HTML</span>
+                        </a>
+                        <a href="{{ url('/belajar/css') }}">
+                            <span>Belajar CSS</span>
+                        </a>
+                        <a href="{{ url('/belajar/php') }}">
+                            <span>Belajar PHP</span>
+                        </a>
+                        <a href="{{ url('/belajar/js') }}">
+                            <span>Belajar JS</span>
+                        </a>
+                        <a href="{{ url('/belajar/sql') }}">
+                            <span>Belajar SQL</span>
+                        </a>
                     </div>
                     <div class="col-4">
-                        Belajar Bahasa Pemograman
+                        <h6>
+                            Latihan
+                        </h6>
+                        <a href="{{ url('/latihan/html/basic') }}">
+                            <span>Latihan HTML</span>
+                        </a>
+                        <a href="{{ url('/latihan/css/basic') }}">
+                            <span>Latihan CSS</span>
+                        </a>
+                        <a href="{{ url('/latihan/php/basic') }}">
+                            <span>Latihan PHP</span>
+                        </a>
+                        <a href="{{ url('/latihan/js/basic') }}">
+                            <span>Latihan JS</span>
+                        </a>
+                        <a href="{{ url('/latihan/sql/basic') }}">
+                            <span>Latihan SQL</span>
+                        </a>
                     </div>
                     <div class="col-4">
-                        Belajar dari Para Mentor
-                    </div>
-                    <div class="col-4">
-                        Latihan
+                        <h6>
+                            Belajar dari Para Mentor
+                        </h6>
+                        <a href="{{ url('/kelas-mentor/html') }}">
+                            <span>Kelas HTML</span>
+                        </a>
+                        <a href="{{ url('/kelas-mentor/css') }}">
+                            <span>Kelas CSS</span>
+                        </a>
+                        <a href="{{ url('/kelas-mentor/php') }}">
+                            <span>Kelas PHP</span>
+                        </a>
+                        <a href="{{ url('/kelas-mentor/js') }}">
+                            <span>Kelas JS</span>
+                        </a>
+                        <a href="{{ url('/kelas-mentor/sql') }}">
+                            <span>Kelas SQL</span>
+                        </a>
                     </div>
                 </div>
             </div>
