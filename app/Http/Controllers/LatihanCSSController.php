38e375d7e75dee3bb8cb9796de01ac3515add61b
Nivea Nyctra <nivea.nyctra@gmail.com>
2023-09-12 13:29:12 +0700

    update latihan css

--- app/Http/Controllers/LatihanCSSController.php
+++ app/Http/Controllers/LatihanCSSController.php
@@ -12,94 +12,49 @@ class LatihanCSSController extends Controller
         $cek3 = $request->jawaban3;
         $cek4 = $request->jawaban4;
         $cek5 = $request->jawaban5;
-        $cek6 = $request->jawaban6;
-        $cek7 = $request->jawaban7;
-        $cek8 = $request->jawaban8;
-        $cek9 = $request->jawaban9;
-        $cek10 = $request->jawaban10;
 
-        if ($cek1 == "center") {
+        if ($cek1 == "p") {
             # code...
             $cek1 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek1 = "Jawaban Anda Salah, Jawaban Yang Benar : center";
+            $cek1 = "Jawaban Anda Salah, Jawaban Yang Benar : p";
         }
-        if ($cek2 == "right") {
+        if ($cek2 == ".tanda") {
             # code...
             $cek2 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek2 = "Jawaban Anda Salah, Jawaban Yang Benar : right";
+            $cek2 = "Jawaban Anda Salah, Jawaban Yang Benar : .tanda";
         }
-        if ($cek3 == "src") {
+        if ($cek3 == "#tanda") {
             # code...
             $cek3 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek3 = "Jawaban Anda Salah, Jawaban Yang Benar : src";
+            $cek3 = "Jawaban Anda Salah, Jawaban Yang Benar : #tanda";
         }
-        if ($cek4 == "height") {
+        if ($cek4 == "p, h1") {
             # code...
             $cek4 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek4 = "Jawaban Anda Salah, Jawaban Yang Benar : height";
+            $cek4 = "Jawaban Anda Salah, Jawaban Yang Benar : p, h1";
         }
-        if ($cek5 == "width") {
+        if ($cek5 == ":") {
             # code...
             $cek5 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek5 = "Jawaban Anda Salah, Jawaban Yang Benar : width";
-        }
-        if ($cek6 == "color") {
-            # code...
-            $cek6 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek6 = "Jawaban Anda Salah, Jawaban Yang Benar : color";
-        }
-        if ($cek7 == "12pt") {
-            # code...
-            $cek7 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek7 = "Jawaban Anda Salah, Jawaban Yang Benar : 12pt";
-        }
-        if ($cek8 == "href") {
-            # code...
-            $cek8 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek8 = "Jawaban Anda Salah, Jawaban Yang Benar : href";
-        }
-        if ($cek9 == "h6") {
-            # code...
-            $cek9 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek9 = "Jawaban Anda Salah, Jawaban Yang Benar : h6";
-        }
-        if ($cek10 == "blue") {
-            # code...
-            $cek10 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek10 = "Jawaban Anda Salah, Jawaban Yang Benar : blue";
+            $cek5 = "Jawaban Anda Salah, Jawaban Yang Benar : ':'";
         }
         $jawab1 = $request->jawaban1;
         $jawab2 = $request->jawaban2;
         $jawab3 = $request->jawaban3;
         $jawab4 = $request->jawaban4;
         $jawab5 = $request->jawaban5;
-        $jawab6 = $request->jawaban6;
-        $jawab7 = $request->jawaban7;
-        $jawab8 = $request->jawaban8;
-        $jawab9 = $request->jawaban9;
-        $jawab10 = $request->jawaban10;
-        return view('latihan.css.hasil.hasil-basic',compact('cek1','cek2','cek3','cek4','cek5','cek6','cek7','cek8','cek9','cek10',
-        'jawab1','jawab2','jawab3','jawab4','jawab5','jawab6','jawab7','jawab8','jawab9','jawab10'));
+        return view('latihan.css.hasil.hasil-basic',compact('cek1','cek2','cek3','cek4','cek5',
+        'jawab1','jawab2','jawab3','jawab4','jawab5'));
         // $cek = $cek1."-".$cek2."-".$cek3."-".$cek4."-".$cek5;
         // $rep = str_replace('-', ' ', $cek);
         // $cekjawab = str_word_count($rep);
@@ -111,94 +66,49 @@ class LatihanCSSController extends Controller
         $cek3 = $request->jawaban3;
         $cek4 = $request->jawaban4;
         $cek5 = $request->jawaban5;
-        $cek6 = $request->jawaban6;
-        $cek7 = $request->jawaban7;
-        $cek8 = $request->jawaban8;
-        $cek9 = $request->jawaban9;
-        $cek10 = $request->jawaban10;
 
-        if ($cek1 == "center") {
+        if ($cek1 == "background-color") {
             # code...
             $cek1 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek1 = "Jawaban Anda Salah, Jawaban Yang Benar : center";
+            $cek1 = "Jawaban Anda Salah, Jawaban Yang Benar : background-color";
         }
-        if ($cek2 == "right") {
+        if ($cek2 == 'url("gunung.png")') {
             # code...
             $cek2 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek2 = "Jawaban Anda Salah, Jawaban Yang Benar : right";
+            $cek2 = 'Jawaban Anda Salah, Jawaban Yang Benar : url("gunung.png")';
         }
-        if ($cek3 == "src") {
+        if ($cek3 == "padding-top") {
             # code...
             $cek3 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek3 = "Jawaban Anda Salah, Jawaban Yang Benar : src";
+            $cek3 = "Jawaban Anda Salah, Jawaban Yang Benar : padding-top";
         }
-        if ($cek4 == "height") {
+        if ($cek4 == "width") {
             # code...
             $cek4 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek4 = "Jawaban Anda Salah, Jawaban Yang Benar : height";
+            $cek4 = "Jawaban Anda Salah, Jawaban Yang Benar : width";
         }
-        if ($cek5 == "width") {
+        if ($cek5 == "none") {
             # code...
             $cek5 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek5 = "Jawaban Anda Salah, Jawaban Yang Benar : width";
-        }
-        if ($cek6 == "color") {
-            # code...
-            $cek6 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek6 = "Jawaban Anda Salah, Jawaban Yang Benar : color";
-        }
-        if ($cek7 == "12pt") {
-            # code...
-            $cek7 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek7 = "Jawaban Anda Salah, Jawaban Yang Benar : 12pt";
-        }
-        if ($cek8 == "href") {
-            # code...
-            $cek8 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek8 = "Jawaban Anda Salah, Jawaban Yang Benar : href";
-        }
-        if ($cek9 == "h6") {
-            # code...
-            $cek9 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek9 = "Jawaban Anda Salah, Jawaban Yang Benar : h6";
-        }
-        if ($cek10 == "blue") {
-            # code...
-            $cek10 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek10 = "Jawaban Anda Salah, Jawaban Yang Benar : blue";
+            $cek5 = "Jawaban Anda Salah, Jawaban Yang Benar : none";
         }
         $jawab1 = $request->jawaban1;
         $jawab2 = $request->jawaban2;
         $jawab3 = $request->jawaban3;
         $jawab4 = $request->jawaban4;
         $jawab5 = $request->jawaban5;
-        $jawab6 = $request->jawaban6;
-        $jawab7 = $request->jawaban7;
-        $jawab8 = $request->jawaban8;
-        $jawab9 = $request->jawaban9;
-        $jawab10 = $request->jawaban10;
-        return view('latihan.css.hasil.hasil-menengah',compact('cek1','cek2','cek3','cek4','cek5','cek6','cek7','cek8','cek9','cek10',
-        'jawab1','jawab2','jawab3','jawab4','jawab5','jawab6','jawab7','jawab8','jawab9','jawab10'));
+        return view('latihan.css.hasil.hasil-menengah',compact('cek1','cek2','cek3','cek4','cek5',
+        'jawab1','jawab2','jawab3','jawab4','jawab5'));
     }
     public function cekCSSLanjutan(Request $request){
         $cek1 = $request->jawaban1;
@@ -206,93 +116,48 @@ class LatihanCSSController extends Controller
         $cek3 = $request->jawaban3;
         $cek4 = $request->jawaban4;
         $cek5 = $request->jawaban5;
-        $cek6 = $request->jawaban6;
-        $cek7 = $request->jawaban7;
-        $cek8 = $request->jawaban8;
-        $cek9 = $request->jawaban9;
-        $cek10 = $request->jawaban10;
 
-        if ($cek1 == "center") {
+        if ($cek1 == "20px 40px") {
             # code...
             $cek1 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek1 = "Jawaban Anda Salah, Jawaban Yang Benar : center";
+            $cek1 = "Jawaban Anda Salah, Jawaban Yang Benar : 20px 40px";
         }
-        if ($cek2 == "right") {
+        if ($cek2 == "12px 21px 15px 20px") {
             # code...
             $cek2 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek2 = "Jawaban Anda Salah, Jawaban Yang Benar : right";
+            $cek2 = "Jawaban Anda Salah, Jawaban Yang Benar : 12px 21px 15px 20px";
         }
-        if ($cek3 == "src") {
+        if ($cek3 == "repeat-y") {
             # code...
             $cek3 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek3 = "Jawaban Anda Salah, Jawaban Yang Benar : src";
+            $cek3 = "Jawaban Anda Salah, Jawaban Yang Benar : repeat-y";
         }
-        if ($cek4 == "height") {
+        if ($cek4 == "2px solid black") {
             # code...
             $cek4 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek4 = "Jawaban Anda Salah, Jawaban Yang Benar : height";
+            $cek4 = "Jawaban Anda Salah, Jawaban Yang Benar : 2px solid black";
         }
-        if ($cek5 == "width") {
+        if ($cek5 == "border-radius") {
             # code...
             $cek5 = "Jawaban Anda Benar";
         }else {
             # code...
-            $cek5 = "Jawaban Anda Salah, Jawaban Yang Benar : width";
-        }
-        if ($cek6 == "color") {
-            # code...
-            $cek6 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek6 = "Jawaban Anda Salah, Jawaban Yang Benar : color";
-        }
-        if ($cek7 == "12pt") {
-            # code...
-            $cek7 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek7 = "Jawaban Anda Salah, Jawaban Yang Benar : 12pt";
-        }
-        if ($cek8 == "href") {
-            # code...
-            $cek8 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek8 = "Jawaban Anda Salah, Jawaban Yang Benar : href";
-        }
-        if ($cek9 == "h6") {
-            # code...
-            $cek9 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek9 = "Jawaban Anda Salah, Jawaban Yang Benar : h6";
-        }
-        if ($cek10 == "blue") {
-            # code...
-            $cek10 = "Jawaban Anda Benar";
-        }else {
-            # code...
-            $cek10 = "Jawaban Anda Salah, Jawaban Yang Benar : blue";
+            $cek5 = "Jawaban Anda Salah, Jawaban Yang Benar : border-radius";
         }
         $jawab1 = $request->jawaban1;
         $jawab2 = $request->jawaban2;
         $jawab3 = $request->jawaban3;
         $jawab4 = $request->jawaban4;
         $jawab5 = $request->jawaban5;
-        $jawab6 = $request->jawaban6;
-        $jawab7 = $request->jawaban7;
-        $jawab8 = $request->jawaban8;
-        $jawab9 = $request->jawaban9;
-        $jawab10 = $request->jawaban10;
-        return view('latihan.css.hasil.hasil-lanjutan',compact('cek1','cek2','cek3','cek4','cek5','cek6','cek7','cek8','cek9','cek10',
-        'jawab1','jawab2','jawab3','jawab4','jawab5','jawab6','jawab7','jawab8','jawab9','jawab10'));
+        return view('latihan.css.hasil.hasil-lanjutan',compact('cek1','cek2','cek3','cek4','cek5',
+        'jawab1','jawab2','jawab3','jawab4','jawab5'));
     }
 }
--- resources/views/layouts/latihan/sidebar/css.blade.php
+++ resources/views/layouts/latihan/sidebar/css.blade.php
@@ -7,33 +7,18 @@
             <div onclick="soal3()" class="ps-5 link basic" style="display:none;">Soal 3</div>
             <div onclick="soal4()" class="ps-5 link basic" style="display:none;">Soal 4</div>
             <div onclick="soal5()" class="ps-5 link basic" style="display:none;">Soal 5</div>
-            <div onclick="soal6()" class="ps-5 link basic" style="display:none;">Soal 6</div>
-            <div onclick="soal7()" class="ps-5 link basic" style="display:none;">Soal 7</div>
-            <div onclick="soal8()" class="ps-5 link basic" style="display:none;">Soal 8</div>
-            <div onclick="soal9()" class="ps-5 link basic" style="display:none;">Soal 9</div>
-            <div onclick="soal10()" class="ps-5 link basic" style="display:none;">Soal 10</div>
             <a href="{{url('/latihan/css/menengah')}}" class="menengahNav {{ request()->is('latihan/css/menengah') ? 'active' : '' }} {{ request()->is('hasil/css/menengah') ? 'active' : '' }}">CSS Menengah</a>
             <div onclick="soal1()" class="ps-5 link menengah" style="display:none;">Soal 1</div>
             <div onclick="soal2()" class="ps-5 link menengah" style="display:none;">Soal 2</div>
             <div onclick="soal3()" class="ps-5 link menengah" style="display:none;">Soal 3</div>
             <div onclick="soal4()" class="ps-5 link menengah" style="display:none;">Soal 4</div>
             <div onclick="soal5()" class="ps-5 link menengah" style="display:none;">Soal 5</div>
-            <div onclick="soal6()" class="ps-5 link menengah" style="display:none;">Soal 6</div>
-            <div onclick="soal7()" class="ps-5 link menengah" style="display:none;">Soal 7</div>
-            <div onclick="soal8()" class="ps-5 link menengah" style="display:none;">Soal 8</div>
-            <div onclick="soal9()" class="ps-5 link menengah" style="display:none;">Soal 9</div>
-            <div onclick="soal10()" class="ps-5 link menengah" style="display:none;">Soal 10</div>
             <a href="{{url('/latihan/css/lanjutan')}}" class="lanjutanNav {{ request()->is('latihan/css/lanjutan') ? 'active' : '' }} {{ request()->is('hasil/css/lanjutan') ? 'active' : '' }}">CSS Lanjutan</a>
             <div onclick="soal1()" class="ps-5 link lanjutan" style="display:none;">Soal 1</div>
             <div onclick="soal2()" class="ps-5 link lanjutan" style="display:none;">Soal 2</div>
             <div onclick="soal3()" class="ps-5 link lanjutan" style="display:none;">Soal 3</div>
             <div onclick="soal4()" class="ps-5 link lanjutan" style="display:none;">Soal 4</div>
             <div onclick="soal5()" class="ps-5 link lanjutan" style="display:none;">Soal 5</div>
-            <div onclick="soal6()" class="ps-5 link lanjutan" style="display:none;">Soal 6</div>
-            <div onclick="soal7()" class="ps-5 link lanjutan" style="display:none;">Soal 7</div>
-            <div onclick="soal8()" class="ps-5 link lanjutan" style="display:none;">Soal 8</div>
-            <div onclick="soal9()" class="ps-5 link lanjutan" style="display:none;">Soal 9</div>
-            <div onclick="soal10()" class="ps-5 link lanjutan" style="display:none;">Soal 10</div>
             <a href="{{ url('/belajar/css') }}"><span>Pergi ke CSS Tutorial</span></a>
     </nav>
 </aside>
