7d2eb2a952bbe8fca61658439a1c3167266bc5e6
Nivea Nyctra <113758640+niveanyctra@users.noreply.github.com>
2023-09-18 09:17:18 +0700

    search no

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,28 +4,11 @@ namespace App\Models;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
-use Nicolaslopezj\Searchable\SearchableTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory, SearchableTrait;
-
-    protected $searchable = [
-        'columns' => [
-            'posts.title',
-            'posts.category',
-            'posts.level',
-            'posts.tool',
-            'posts.about',
-            'users.name' ,
-            'users.username' ,
-            'users.email' ,
-        ],
-        'joins' => [
-            'users' => ['post.id_user','users.id'],
-        ],
-    ];
+    use HasFactory;
 
     protected $table = 'posts';
     protected $primaryKey = 'id';
