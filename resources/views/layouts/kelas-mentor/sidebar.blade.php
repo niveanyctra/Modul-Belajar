fccc91b48f6b820579e38bba9da05ee3b0123e6e
Nivea Nyctra <rizki.adsu@gmail.com>
2023-09-28 11:44:08 +0700

    update sidebar mentor responsive (belum bener)

--- resources/views/layouts/kelas-mentor/sidebar.blade.php
+++ resources/views/layouts/kelas-mentor/sidebar.blade.php
@@ -2,9 +2,7 @@
     <form method="post" action="" >
         <h3 class="py-2 fw-bolder">Urutkan</h3>
             <div class="form-check">
-                {{-- <label for=""> --}}
-                    <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="noCheck" checked>
-                {{-- </label> --}}
+                <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="noCheck" checked>
                 <label class="form-check-label mb-2" style="display: block;" for="noCheck">
                     Semua
                 </label>
@@ -23,18 +21,30 @@
             </div>
     </form>
 </div>
-
-{{-- <div class="col-lg-2 col-12 d-none d-lg-block course-filter">
-    <div id="content" class="mt-30 follow-scroll" style="position: relative; transition: all 600ms linear 0s;">
-        <div class="item-filter">
-            <h3 class="head">Sort</h3>
-            <div class="child">
-                <label for="" class="container-inp">
-                    <input type="radio" name="sort" autocomplete="off" value="recent" id="reRelease" class="nd-checkbox">
-                </label>
-                <label for="reRelease" class="label"> Baru Rilis </label>
-            </div>
+<div class="modal bg-white" tabindex="-1" aria-labelledby="..." aria-hidden="true" id="options">
+    <form action="?level=" method="post">
+        @csrf
+        <h3 class="py-2 fw-bolder">Urutkan</h3>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="noCheck" checked>
+            <label class="form-check-label mb-2" style="display: block;" for="noCheck">
+                Semua
+            </label>
         </div>
-    </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="mudahCheck">
+            <label class="form-check-label mb-2"  style="display: block;" for="mudahCheck">
+                Mudah
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="menengahCheck">
+            <label class="form-check-label mb-2"  style="display: block;" for="menengahCheck">
+                Menengah
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Filter</button>
+    </form>
 </div>
-<script src="https://buildwithangga.com/js/catalogue-courses.js"></script> --}}
+<a id="btn-filter" href="#" class="btn btn-secondary btn-filter d-block d-xl-none" data-bs-toggle="modal" data-bs-target="#options">Filter Catalog</a>
+
