fec5f493f2918d7cda4e85e69d7c425083b1799c
RafRizu <thisrafi10@gmail.com>
2023-09-28 12:23:12 +0700

    fix responsive filter kelas mentor

--- resources/views/layouts/kelas-mentor/sidebar.blade.php
+++ resources/views/layouts/kelas-mentor/sidebar.blade.php
@@ -22,23 +22,22 @@
     </form>
 </div>
 <div class="modal bg-white" tabindex="-1" aria-labelledby="..." aria-hidden="true" id="options">
-    <form action="?level=" method="post">
-        @csrf
+    <form action="" method="get">
         <h3 class="py-2 fw-bolder">Urutkan</h3>
         <div class="form-check">
-            <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="noCheck" checked>
+            <input class="form-check-input" type="radio" name="level" value="" id="noCheck" checked>
             <label class="form-check-label mb-2" style="display: block;" for="noCheck">
                 Semua
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="mudahCheck">
+            <input class="form-check-input" type="radio" name="level" value="mudah" id="mudahCheck">
             <label class="form-check-label mb-2"  style="display: block;" for="mudahCheck">
                 Mudah
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" onclick="javascript:levelCheck();" name="level" id="menengahCheck">
+            <input class="form-check-input" type="radio" name="level" value="menengah" id="menengahCheck">
             <label class="form-check-label mb-2"  style="display: block;" for="menengahCheck">
                 Menengah
             </label>
