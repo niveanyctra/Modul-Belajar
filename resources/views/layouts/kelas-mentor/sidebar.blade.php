a7b59c2ffc1361788462427de951f801c0625dc5
Hamadah2O2 <muhammadrozaan@gmail.com>
2023-10-05 09:44:43 +0700

    FilterMobile Fix

--- resources/views/layouts/kelas-mentor/sidebar.blade.php
+++ resources/views/layouts/kelas-mentor/sidebar.blade.php
@@ -21,31 +21,38 @@
             </div>
     </form>
 </div>
-<div class="modal bg-white" tabindex="-1" aria-labelledby="..." aria-hidden="true" id="options">
-    <div class="container">
-        <form action="" method="get">
-            <h3 class="py-2 fw-bolder">Tingkatan Level</h3>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="level" value="" id="noCheck" checked>
-                <label class="form-check-label mb-2" style="display: block;" for="noCheck">
-                    Semua
-                </label>
+<div class="modal modal-fullscreen bg-white" tabindex="-1" aria-labelledby="..." aria-hidden="true" id="options">
+    <!-- <div class="container"> -->
+        <div class="modal-dialog">
+            <div class="modal-header">
+                <h3 class="py-2 fw-bolder">Tingkatan Level</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="level" value="mudah" id="mudahCheck">
-                <label class="form-check-label mb-2"  style="display: block;" for="mudahCheck">
-                    Mudah
-                </label>
+            <div class="modal-body container">
+                <form action="" method="get">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="level" value="" id="noCheck" checked>
+                        <label class="form-check-label mb-2" style="display: block;" for="noCheck">
+                            Semua
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="level" value="mudah" id="mudahCheck">
+                        <label class="form-check-label mb-2"  style="display: block;" for="mudahCheck">
+                            Mudah
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="level" value="menengah" id="menengahCheck">
+                        <label class="form-check-label mb-2"  style="display: block;" for="menengahCheck">
+                            Menengah
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-filter">Filter</button>
+                </form>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="level" value="menengah" id="menengahCheck">
-                <label class="form-check-label mb-2"  style="display: block;" for="menengahCheck">
-                    Menengah
-                </label>
-            </div>
-            <button type="submit" class="btn btn-primary">Filter</button>
-        </form>
-    </div>
+        </div>
+    <!-- </div> -->
 </div>
 <a id="btn-filter" href="#" class="btn btn-secondary btn-filter d-block d-lg-none" data-bs-toggle="modal" data-bs-target="#options">Filter Catalog</a>
 
